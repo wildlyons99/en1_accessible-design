a2cba385f2420381b09c044a93e45dc438c7b617
wildlyons99 <57906707+wildlyons99@users.noreply.github.com>
2021-11-29 20:48:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# EN1: final project template files
+
+Template code for Dr. E's EN1 (Simple Robotics) Final Project
+
+## Students:
+
+You should use this repository as a Template (see button in top right) and copy it to your personal GitHub account.
+
+See Canvas for more instructions.
---
+++ src/secret.js
@@ -0,0 +1,23 @@
+
+////////////////////////////
+// THESE ARE YOUR SECRETS //
+////////////////////////////
+
+// This is how you connect to the Cloud database
+// There are a few different ways to do this.
+
+// (1) Using the EN1 Simple Robotics API Storage tool
+// - using this means you don't need to store your
+//   Cloud API Key in your code/within GitHub
+// - if you leave this blank ("") then code assumes you
+//   are using method #2 below
+var getAPIusername = ""; // fill in with your name (if left blank, assuming using method #2)
+
+// (2) Hardcode your API key information here
+
+// AIRTABLE SUPPORT
+var airtable_api = "key" + "XXXXXXXXXXXXXX";     // fill in your info
+var airtable_baseid = "app" + "YYYYYYYYYYYYYY";  // fill in your info
+
+// SYSTEMLINK SUPPORT
+var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
